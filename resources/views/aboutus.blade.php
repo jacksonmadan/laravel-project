2f614683b14a59cf65b61e46a59ca1ea043549dc
jacksonmadan <ankushmadanapple@gmail.com>
2024-09-16 01:21:32 +0530

    new3

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-hello hows u
+<p style="color: white;">hello i am ankush</p>
 <div class="container" style="margin-top: 40px;">
 <h1 style="text-align: center; color: #ffffff; margin-bottom: 30px;">About Us</h1>
 hi bro this is checking for CI/CD
